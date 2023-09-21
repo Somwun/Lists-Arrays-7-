f6b66342958d943226909d9403c401eb3f88643d
Christopher Staddon <c06staddon@gmail.com>
2023-09-21 12:00:09 -0400

    Add project files.

---
+++ Lists & Arrays (#7)/Program.cs
@@ -0,0 +1,18 @@
+﻿using static System.Formats.Asn1.AsnWriter;
+
+namespace Lists___Arrays___7_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> randomNumbers = new List<int>();
+            Random generator = new Random();
+            for (int i = 0; i < 25; i ++)
+            {
+                randomNumbers.Add(generator.Next(10,21));
+                Console.Write(randomNumbers[i] + " ");
+            }
+        }
+    }
+}
