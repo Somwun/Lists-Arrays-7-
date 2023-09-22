1070334955a53fd4842096edd674d8f3a6469a20
Christopher Staddon <c06staddon@gmail.com>
2023-09-22 11:02:11 -0400

    h

--- Lists & Arrays (#7)/Program.cs
+++ Lists & Arrays (#7)/Program.cs
@@ -1,4 +1,5 @@
-﻿using static System.Formats.Asn1.AsnWriter;
+﻿using System.ComponentModel.Design;
+using static System.Formats.Asn1.AsnWriter;
 
 namespace Lists___Arrays___7_
 {
@@ -6,14 +7,16 @@ namespace Lists___Arrays___7_
     {
         static void Main(string[] args)
         {
-            int userRemove, userChoice;
+            int userChoice = 2, userAdd, userRemove;
             List<int> randomNumbers = new List<int>();
             Random generator = new Random();
-            bool repeat = true, newNumbers = true, menuOn = true, sort = false;
+            bool repeat = true;
             while (repeat)
             {
-                if (newNumbers)
+                if (userChoice == 2)
                 {
+                    Console.Clear();
+                    randomNumbers.Clear();
                     for (int i = 0; i < 25; i ++)
                     {
                         randomNumbers.Add(generator.Next(10,21));
@@ -21,78 +24,75 @@ namespace Lists___Arrays___7_
                     }
                     newNumbers = false;
                 }
-                while (menuOn)
+                Console.WriteLine("\n\nPick what you'd like to do with the string of numbers above\n(Type the number of the corresponding action)\n1. Sort the list\n2. Regenorate list\n3. Remove one of the numbers\n4. Add a number\n5. Count the numbers\n6. Show the biggest number\n7. Show the smallest number\n8. Show the average number\n9. Show the number that shows up the most\n10. Quit");
+                while (!int.TryParse(Console.ReadLine(), out userChoice) || userChoice < 1 || userChoice > 11)
                 {
-                    Console.WriteLine("\n\nPick what you'd like to do with the string of numbers above\nType the number of the corresponding action\n1. Sort the list\n2. Regenorate list\n3. Remove one of the numbers\n4. Add a number\n5. Count the numbers\n6. Show the biggest number\n7. Show the smallest number\n8. Show the average number\n9. Show the number that shows up the most\n10. Quit");
-                    while (!int.TryParse(Console.ReadLine(), out userChoice) || userChoice < 1 || userChoice > 11)
-                    {
-                        Console.WriteLine("Not quite, pick something else");
-                    }
-                    switch(userChoice)
-                    {
-                        case 1:
-
-                            break;
-                            break;
-                        case 2:
-
-                            break;
-                            break;
-                        case 3:
-
-                            break;
-                            break;
-                        case 4:
-
-                            break;
-                            break;
-                        case 5:
-
-                            break;
-                            break;
-                        case 6:
-
-                            break;
-                            break;
-                        case 7:
-
-                            break;
-                            break;
-                        case 8:
-
-                            break;
-                        case 9:
-
-                            break;
-                            break;
-                        case 10:
-
-                            break;
-                            break;
-                    }
+                    Console.WriteLine("Not quite, pick something else");
                 }
-                Console.WriteLine("");
-                randomNumbers.Sort();
-                for (int i = 0; i < 25; i++)
+                switch (userChoice)
                 {
-                    Console.Write(randomNumbers[i] + " ");
-                }
-                Console.WriteLine("\nPick a number to remove");
-                while (!int.TryParse(Console.ReadLine(), out userRemove) || userRemove < 10 || userRemove > 20)
-                {
-                    Console.WriteLine("Pick something else");
-                }
-                Console.WriteLine("");
-                while (randomNumbers.Contains(userRemove))
-                {
-                    randomNumbers.Remove(userRemove);
-                }
-                for (int i = 0; i < randomNumbers.Count(); i ++)
-                {
-                    Console.Write(randomNumbers[i] + " ");
+                    case 1:
+                        Console.Clear();
+                        randomNumbers.Sort();
+                        for (int i = 0; i < randomNumbers.Count(); i++)
+                        {
+                            Console.Write(randomNumbers[i] + " ");
+                        }
+                        break;
+                    case 3:
+                        Console.WriteLine("\nPick a number to remove");
+                        while (!int.TryParse(Console.ReadLine(), out userRemove) || !randomNumbers.Contains(userRemove))
+                        {
+                            if (!randomNumbers.Contains(userRemove))
+                                Console.WriteLine("That number doesn't exit");
+                            else
+                                Console.WriteLine("Pick something else");
+                        }
+                        Console.WriteLine("");
+                        while (randomNumbers.Contains(userRemove))
+                        {
+                            randomNumbers.Remove(userRemove);
+                        }
+                        Console.Clear();
+                        for (int i = 0; i < randomNumbers.Count(); i ++)
+                        {
+                            Console.Write(randomNumbers[i] + " ");
+                        }
+                        break;
+                    case 4:
+                        Console.Clear();
+                        Console.WriteLine("Choose a number to add to the list");
+                        while (!int.TryParse(Console.ReadLine(), out userAdd))
+                        {
+                            Console.WriteLine("Not quite, pick something else");
+                        }
+                        Console.Clear();
+                        randomNumbers.Add(userAdd);
+                        for (int i = 0; i < randomNumbers.Count(); i ++)
+                        {
+                            Console.Write(randomNumbers[i] + " ");
+                        }
+                        break;
+                    case 5:
+
+                        break;
+                    case 6:
+
+                        break;
+                    case 7:
+
+                        break;
+                    case 8:
+
+                        break;
+                    case 9:
+
+                        break;
+                    case 10:
+
+                        break;
                 }
             }
-
         }
     }
 }
