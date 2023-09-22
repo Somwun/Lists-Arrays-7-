b4648a3068e7b1df27dcda7cf52fbf72dbfd6e80
Christopher Staddon <c06staddon@gmail.com>
2023-09-22 10:19:07 -0400

    h

--- Lists & Arrays (#7)/Program.cs
+++ Lists & Arrays (#7)/Program.cs
@@ -6,13 +6,93 @@ namespace Lists___Arrays___7_
     {
         static void Main(string[] args)
         {
+            int userRemove, userChoice;
             List<int> randomNumbers = new List<int>();
             Random generator = new Random();
-            for (int i = 0; i < 25; i ++)
+            bool repeat = true, newNumbers = true, menuOn = true, sort = false;
+            while (repeat)
             {
-                randomNumbers.Add(generator.Next(10,21));
-                Console.Write(randomNumbers[i] + " ");
+                if (newNumbers)
+                {
+                    for (int i = 0; i < 25; i ++)
+                    {
+                        randomNumbers.Add(generator.Next(10,21));
+                        Console.Write(randomNumbers[i] + " ");
+                    }
+                    newNumbers = false;
+                }
+                while (menuOn)
+                {
+                    Console.WriteLine("\n\nPick what you'd like to do with the string of numbers above\nType the number of the corresponding action\n1. Sort the list\n2. Regenorate list\n3. Remove one of the numbers\n4. Add a number\n5. Count the numbers\n6. Show the biggest number\n7. Show the smallest number\n8. Show the average number\n9. Show the number that shows up the most\n10. Quit");
+                    while (!int.TryParse(Console.ReadLine(), out userChoice) || userChoice < 1 || userChoice > 11)
+                    {
+                        Console.WriteLine("Not quite, pick something else");
+                    }
+                    switch(userChoice)
+                    {
+                        case 1:
+
+                            break;
+                            break;
+                        case 2:
+
+                            break;
+                            break;
+                        case 3:
+
+                            break;
+                            break;
+                        case 4:
+
+                            break;
+                            break;
+                        case 5:
+
+                            break;
+                            break;
+                        case 6:
+
+                            break;
+                            break;
+                        case 7:
+
+                            break;
+                            break;
+                        case 8:
+
+                            break;
+                        case 9:
+
+                            break;
+                            break;
+                        case 10:
+
+                            break;
+                            break;
+                    }
+                }
+                Console.WriteLine("");
+                randomNumbers.Sort();
+                for (int i = 0; i < 25; i++)
+                {
+                    Console.Write(randomNumbers[i] + " ");
+                }
+                Console.WriteLine("\nPick a number to remove");
+                while (!int.TryParse(Console.ReadLine(), out userRemove) || userRemove < 10 || userRemove > 20)
+                {
+                    Console.WriteLine("Pick something else");
+                }
+                Console.WriteLine("");
+                while (randomNumbers.Contains(userRemove))
+                {
+                    randomNumbers.Remove(userRemove);
+                }
+                for (int i = 0; i < randomNumbers.Count(); i ++)
+                {
+                    Console.Write(randomNumbers[i] + " ");
+                }
             }
+
         }
     }
 }
