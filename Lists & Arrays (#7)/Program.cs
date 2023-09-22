913e96b5c34e9b065971d923c7e430760682f536
Christopher Staddon <c06staddon@gmail.com>
2023-09-22 11:49:43 -0400

    h

--- Lists & Arrays (#7)/Program.cs
+++ Lists & Arrays (#7)/Program.cs
@@ -7,10 +7,10 @@ namespace Lists___Arrays___7_
     {
         static void Main(string[] args)
         {
-            int userChoice = 2, userAdd, userRemove;
+            int userChoice = 2, userAdd, userRemove, userCount, userCountAmount = 0;
             List<int> randomNumbers = new List<int>();
             Random generator = new Random();
-            bool repeat = true;
+            bool repeat = true, count = true;
             while (repeat)
             {
                 if (userChoice == 2)
@@ -22,9 +22,8 @@ namespace Lists___Arrays___7_
                         randomNumbers.Add(generator.Next(10,21));
                         Console.Write(randomNumbers[i] + " ");
                     }
-                    newNumbers = false;
                 }
-                Console.WriteLine("\n\nPick what you'd like to do with the string of numbers above\n(Type the number of the corresponding action)\n1. Sort the list\n2. Regenorate list\n3. Remove one of the numbers\n4. Add a number\n5. Count the numbers\n6. Show the biggest number\n7. Show the smallest number\n8. Show the average number\n9. Show the number that shows up the most\n10. Quit");
+                Console.WriteLine("\n\nPick what you'd like to do with the string of numbers above\n(Type the number of the corresponding action)\n1. Sort the list\n2. Regenorate list\n3. Remove one of the numbers\n4. Add a number\n5. Count occurance of numbers\n6. Show the biggest number\n7. Show the smallest number\n8. Show the average number\n9. Show the number that shows up the most\n10. Quit");
                 while (!int.TryParse(Console.ReadLine(), out userChoice) || userChoice < 1 || userChoice > 11)
                 {
                     Console.WriteLine("Not quite, pick something else");
@@ -41,6 +40,10 @@ namespace Lists___Arrays___7_
                         break;
                     case 3:
                         Console.Clear();
+                        for (int i = 0; i < randomNumbers.Count(); i ++)
+                        {
+                            Console.Write(randomNumbers[i] + " ");
+                        }
                         Console.WriteLine("\nPick a number to remove");
                         while (!int.TryParse(Console.ReadLine(), out userRemove) || !randomNumbers.Contains(userRemove))
                         {
@@ -62,7 +65,11 @@ namespace Lists___Arrays___7_
                         break;
                     case 4:
                         Console.Clear();
-                        Console.WriteLine("Choose a number to add to the list");
+                        for (int i = 0; i < randomNumbers.Count(); i++)
+                        {
+                            Console.Write(randomNumbers[i] + " ");
+                        }
+                        Console.WriteLine("\nChoose a number to add to the list");
                         while (!int.TryParse(Console.ReadLine(), out userAdd))
                         {
                             Console.WriteLine("Not quite, pick something else");
@@ -75,7 +82,36 @@ namespace Lists___Arrays___7_
                         }
                         break;
                     case 5:
-
+                        Console.Clear();
+                        for (int i = 0; i < randomNumbers.Count(); i++)
+                        {
+                            Console.Write(randomNumbers[i] + " ");
+                        }
+                        Console.WriteLine("\nPick a number from the list and I'll tell you how many times it shows up");
+                        while (!int.TryParse(Console.ReadLine(), out userCount) || !randomNumbers.Contains(userCount))
+                        {
+                            if (!randomNumbers.Contains(userCount))
+                                Console.WriteLine("That number doesn't exist");
+                            else
+                                Console.WriteLine("Nope, try again");
+                        }
+                        for (int i = 0; i < randomNumbers.Count(); i ++)
+                        {
+                            if (randomNumbers[i] == userCount)
+                                userCountAmount += 1;
+                        }
+                        randomNumbers.Count();
+                        if (userCountAmount == 1)
+                            Console.WriteLine($"{userCount} shows up 1 time in the list\n(Press enter to continue)");
+                        else
+                            Console.WriteLine($"{userCount} shows up {userCountAmount} times in the list\n(Press enter to continue)");
+                        userCountAmount = 0;
+                        Console.ReadLine();
+                        Console.Clear();
+                        for (int i = 0; i < randomNumbers.Count(); i ++)
+                        {
+                            Console.Write(randomNumbers[i] + " ");
+                        }
                         break;
                     case 6:
 
